9d08dc025af861d320dd69da64ec74fce7c169bc
aashmisivadasank19 <aashmisivadasank@gmail.com>
2026-03-26 17:45:41 +0530

    Add files via upload

---
+++ constructor.cpp
@@ -0,0 +1,25 @@
+#include <iostream.h>
+#include <conio.h>
+class abc
+{
+public:
+int num1, num 2;
+float result;
+
+abc ()
+{
+cout <<" Constructor called";
+}
+void displaydata()
+{
+cout << "displaydata";
+}
+};
+
+void main()
+{
+.abc obj;
+obj. displaydata();
+
+getch;
+}
